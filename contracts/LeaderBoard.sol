98cea3f8cdb05a47904e36688d2b46b7e019763e
aquint-zama <alexandre.quint@zama.ai>
2024-01-25 14:06:33 +0100

    Updated version

--- contracts/LeaderBoard.sol
+++ contracts/LeaderBoard.sol
@@ -6,36 +6,52 @@ import "fhevm/abstracts/Reencrypt.sol";
 import "fhevm/lib/TFHE.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract ConfidentialERC20 is Reencrypt, Ownable2Step {
-    event Transfer(address indexed from, address indexed to);
-    event Approval(address indexed owner, address indexed spender);
-    event Mint(address indexed to, uint32 amount);
-
-    uint32 private _score;
-    uint8 public constant decimals = 0;
+contract EncLeaderBoard is Reencrypt, Ownable2Step {
+    //uint32 private _score;
+    //uint32 private _highestScore=0;
 
     // A mapping from address to an encrypted score.
-    mapping(address => euint32) internal scores;
+    // mapping(address => euint32) internal scores;
+    struct ScoreIndex {
+        uint index;
+        bool isValue;
+    }
 
     // A mapping of the form mapping(owner => mapping(spender => allowance)).
     // mapping(address => mapping(address => euint32)) internal allowances;
-
     constructor() Ownable(msg.sender) {}
+    euint16[512] scores;
+    uint scoreCount = 0;
+    mapping(address => ScoreIndex) indexes;
 
-    // Sets the balance of the owner to the given encrypted balance.
-    function addPoints(uint32 nbPoints) public virtual onlyOwner {
-        scores[owner()] = TFHE.add(scores[owner()], nbPoints);
+    function add_score() public {
+        if (indexes[msg.sender].isValue) {
+            scores[indexes[msg.sender].index] = TFHE.add(scores[indexes[msg.sender].index], TFHE.randEuint8());
+        } else {
+            indexes[msg.sender].isValue = true;
+            indexes[msg.sender].index = scoreCount;
+            scores[indexes[msg.sender].index] = TFHE.asEuint16(TFHE.randEuint8());
+            scoreCount += 1;
+        }
     }
 
-    // Returns the balance of the caller encrypted under the provided public key.
-    function getScore(
-        address wallet,
-        bytes32 publicKey,
-        bytes calldata signature
-    ) public view virtual onlySignedPublicKey(publicKey, signature) returns (bytes memory) {
-        if (wallet == msg.sender) {
-            return TFHE.reencrypt(scores[wallet], publicKey, 0);
+    function user_score(address addr) public view returns (uint16) {
+        return TFHE.decrypt(scores[indexes[addr].index]);
+    }
+
+    function user_ranking(address addr) public view returns (uint16) {
+        require(indexes[addr].isValue);
+        uint userScoreIdx = indexes[addr].index;
+
+        euint16 userScore = scores[userScoreIdx];
+        euint16 usersHigherThanUser = TFHE.asEuint16(0);
+        euint16 one = TFHE.asEuint16(1);
+        for (uint i = 0; i < scoreCount; i++) {
+            if (i != userScoreIdx) {
+                ebool isHigher = TFHE.gt(scores[i], userScore);
+                usersHigherThanUser = TFHE.cmux(isHigher, TFHE.add(usersHigherThanUser, one), usersHigherThanUser);
+            }
         }
-        return TFHE.reencrypt(TFHE.asEuint32(0), publicKey, 0);
+        return TFHE.decrypt(usersHigherThanUser);
     }
 }
